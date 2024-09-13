0aa6341a17e37ce59030a2f6ee63d20665b8f7e6
TomTel85 <thomas.telaak@hotmail.de>
2024-09-13 17:18:39 +0200

    customer portal translation

--- web/apps/customerportal/src/locales/de/translations.ts
+++ web/apps/customerportal/src/locales/de/translations.ts
@@ -5,26 +5,31 @@ type NestedPartialAsStrings<T extends object> = {
 };
 
 export const translations: NestedPartialAsStrings<Translations> = {
+  StuStaPay: "TeamFestlichPay",
   TeamFestlichPay: "TeamFestlichPay",
   logout: "Logout",
   login: "Login",
-  userTagUid: "Bändchen-Chip ID",
-  userTagPin: "Bändchen-Chip Pin",
+  userTagUid: "Guthabenkarte-Chip ID",
+  userTagPin: "Guthabenkarte-Chip Pin",
   loginFailed: "Login fehlgeschlagen: {{reason}}.",
   errorLoadingCustomer: "Fehler beim Laden der Kundendaten",
   payoutInfo:
-    "Um dein Restguthaben nach dem Festival zu erhalten, <1>trage bitte deine Bankdaten hier ein</1>. Deine Auszahlung findet voraussichtlich innerhalb eines Monats statt.",
+    "Um dein Restguthaben nach der Veranstaltung zu erhalten, <1>trage bitte deine Bankdaten hier ein</1>. Die erste Auszahlung findet voraussichtlich 3 Wochen nach Veranstaltungsende statt.",
   about: "Impressum",
   contact: "Kontakt",
-  wristbandTagExample: "Beispiel eines Bändchen-Chips",
-  wristbandTagExampleTitle: "Bändchen-Chip Beispiel mit PIN",
+  wristbandTagExample: "Beispiel einer Guthabenkarte",
+  wristbandTagExampleTitle: "Guthabenkarte-Chip Beispiel mit PIN und ID",
   wristbandTagExampleDescription:
-    "Die Bändchen-Chip PIN findest Du auf der Rückseite deines Bändchen-Chips. Sie sollte aussehen wie im unteren Beispiel:",
+    "Die Chip ID und PIN findest Du auf der Rückseite deiner Guthabenkarte.",
   termsAndConditionsHeader: "Die Datenschutzbestimmungen können <1>hier</1> eingesehen werden.",
   privacyPolicyHeader: "Unsere AGBs können <1>hier</1> eingesehen werden.",
   languages: {
     en: "English",
     de: "Deutsch",
+  },
+  transaction:
+  {
+    sepaExit: "Auszahlung (SEPA)"
   },
   nav: {
     payout: "Auszahlung",
@@ -33,8 +38,7 @@ export const translations: NestedPartialAsStrings<Translations> = {
     faq: "FAQ",
   },
   balance: "Guthaben",
-  tagPin: "Bändchen-Chip Pin",
-  tagUid: "Bändchen-Chip Uid",
+  tagUid: "Guthabenkarte-Chip ID",
   vouchers: "Getränkemarken",
   order: {
     loadingError: "Fehler beim laden der Bestellungen",
@@ -52,36 +56,32 @@ export const translations: NestedPartialAsStrings<Translations> = {
       ticket: "Ticketkauf",
     },
   },
-  transaction: {
-    cashExit: "Bar Auszahlung",
-    sepaExit: "Auszahlung per Überweisung",
-    donationExit: "Spende",
-  },
   payout: {
+    infoPayoutInitiated: "Auszahlung iniziert", 
+    infoPayoutScheduled: "Auszahlung wird ausgeführt",
+    infoPayoutCompleted: "Auszahlung erfolgreich",
     iban: "IBAN",
-    bankAccountHolder: "Bankkontoinhaber",
+    bankAccountHolder: "Kontoinhaber",
     email: "E-Mail",
-    info: "Damit wir dein Restguthaben überweisen können, trage bitte deine Bankdaten hier ein. Du kannst auch einen Teil oder Dein gesamtes Guthaben an uns spenden, um unser ehrenamtliches Engagement zu unterstützen. Kulturleben in der Studentenstadt e.V. is ein von Studierenden betriebener gemeinnütziger Verein, welcher jährlich das StuStaCulum-Festival organisiert. Deine Auszahlung findet voraussichtlich innerhalb eines Monats statt.",
-    infoPayoutInitiated: "Du hast deine Bankinformationen bereits angegeben und dein verbleibendes Guthaben wird in der nächsten manuell ausgelösten Auszahlung (in der Regel innerhalb eines Monats) ausgezahlt. Du kannst jedoch weiterhin deine Bankinformationen oder deine Spendenwahl ändern. Vielen Dank für deine Geduld.",
-    infoPayoutScheduled: "Du bist für unsere nächste manuell ausgelöste Auszahlung eingeplant, daher kannst du deine Bankinformationen nicht mehr ändern. Halte durch, wir werden dich benachrichtigen, sobald wir die Banküberweisung von unserer Seite aus initiiert haben.",
-    infoPayoutCompleted: "Vielen Dank für deine Geduld, wir haben die Banküberweisung von unserer Seite aus am {{payout_date}} initiiert. Möglicherweise hast du die Gelder bereits erhalten, andernfalls sollten sie innerhalb der nächsten Tage eintreffen. Du kannst die Überweisungsdetails in der Transaktionsliste auf der Hauptseite einsehen.",
+    info: "Damit wir dein Restguthaben überweisen können, trage bitte Deine Bankdaten hier ein. Wenn Du unser ehrenamtliches Engagement unterstützen möchtest, kannst Du bei dem Punkt „Spendenbetrag“ einen Betrag Deiner Wahl eingegeben. Das System errechnet nach dem Drücken des Buttons „Bankdaten speichern“ dann automatisch den Auszahlungsbetrag. Möchtest Du keinen Betrag spenden bitte einfach nur den Button „Bankdaten speichern“ drücken, der auf der Karte verfügbare Betrag wird dann zur Auszahlung vorgemerkt.Die erste Auszahlung findet voraussichtlich 3 Wochen nach Veranstaltungsende statt.",
     ibanNotValid: "ungültige IBAN",
     countryCodeNotSupported: "IBAN Ländercode wird nicht unterstützt",
     nameHasSpecialChars: "Der Accountname enthält nicht valide Sonderzeichen",
     mustAcceptPrivacyPolicy: "Sie müssen die Datenschutserklärung akzeptieren",
-    privacyPolicyCheck: "Ich habe die <1>Datenschutzerklärung</1> des StuStaCulum gelesen und akzeptiere sie.",
+    privacyPolicyCheck: "Ich habe die <1>Datenschutzerklärung</1> gelesen und akzeptiere sie.",
     errorFetchingData: "Fehler beim laden der Daten.",
     updatedBankData:
-      "Bankdaten erfolgreich aktualisiert. Deine Ausahlung erfolgt voraussichtlich innheralb von einem Monat.",
+      "Bankdaten erfolgreich aktualisiert. Die erste Auszahlung erfolgt voraussichtlich 3 Wochen nach Veranstaltungsende.",
     errorWhileUpdatingBankData: "Fehler beim aktualisieren der Bankdaten.",
     donationMustBePositive: "Das Spende muss positiv sein",
     donationExceedsBalance: "Die Spende darf nicht den Kontostand überschreiten",
     donationTitle: "Spende",
     payoutTitle: "Auszahlung",
     donationAmount: "Spendebetrag ",
+    payoutAmount: "Auszahlungsbetrag ",
     donationDescription:
-      "Hat dir das Festival gefallen? Wir würden uns über eine Spende freuen, um unsere ehrenamtliche Arbeit zu unterstützen und zukünftige StuStaCula noch besser zu machen.",
-    donateRemainingBalanceOf: "Spende verbleibende Summe von {{remainingBalance}}",
+      "Hat dir die Veranstaltung gefallen? Wir würden uns über eine Spende freuen, um unsere ehrenamtliche Arbeit zu unterstützen.",
+    donateRemainingBalanceOf: "Spende verbleibende Summe von ",
     submitPayoutData: "Bankdaten speichern",
     submitPayoutDataEdit: "Bankdaten editieren",
     confirmDonateAllTitle: "Gesamtes Restguthaben spenden?",
@@ -91,7 +91,7 @@ export const translations: NestedPartialAsStrings<Translations> = {
   },
   topup: {
     onlineTopUp: "Online-Aufladung",
-    description: "Du kannst dein Festival-Guthaben hier mit Kreditkarte aufladen.",
+    description: "Du kannst dein Guthaben hier mit Kreditkarte aufladen.",
     amount: "Betrag",
     errorWhileCreatingCheckout: "Fehler beim erstellen der SumUp-Zahlung.",
     errorAmountGreaterZero: "Betrag muss größer als 0 sein.",
